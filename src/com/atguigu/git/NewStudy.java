cde990855d7669b1d709f237ce6457dedada18a6
fuguangyu <18762687963fgy@sina.com>
2017-06-23 08:53:42 +0800

    Signed-off-by: fuguangyu <18762687963fgy@sina.com>

---
+++ src/com/atguigu/git/NewStudy.java
@@ -0,0 +1,23 @@
+package com.atguigu.git;
+
+/**
+*创建时间    2017年6月23日 上午8:53:00
+*项目名称    git_study
+*@author   付光裕
+*@version  1.0
+*@since     JDK 1.7.0_71
+*文件名称:  NewStudy.java
+*类说明:
+*/
+
+public class NewStudy {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("new branch");
+	}
+
+}
+
+
+
